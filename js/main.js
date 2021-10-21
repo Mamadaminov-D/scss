0f57e75edb462e5752bdbd315298a04680102ce8
irwin7 <ravshankhan777@icloud.com>
2021-10-21 09:13:54 +0500

    initial

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 let elCloseBtn = document.querySelector(".site-header__menu")
 let elNav = document.querySelector(".nav")
+let elToggleBtn = document.querySelector(".toggle__btn")
 
 elCloseBtn.addEventListener("click", () => {
   elNav.classList.toggle("open")
+})
+
+elToggleBtn.addEventListener("click", ()=>{
+  elToggleBtn.classList.toggle('left')
 })
